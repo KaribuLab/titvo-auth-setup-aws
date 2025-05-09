75f2c415a6382386bfe4c69d7a299181b466304d
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-05-09 01:29:46 -0400

    fix: Agregar el módulo SecretModule usado por AesService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common'
 import { SetupModule } from '@infrastructure/setup/setup.module'
 import { pino } from 'pino'
 import { LoggerModule } from 'nestjs-pino'
+import { SecretModule } from '@aws/secret'
 @Module({
   imports: [
     SetupModule,
@@ -24,6 +25,10 @@ import { LoggerModule } from 'nestjs-pino'
         tableName: process.env.CONFIG_TABLE_NAME ?? 'api-key-table'
       },
       isGlobal: true
+    }),
+    SecretModule.forRoot({
+      awsStage: process.env.AWS_STAGE ?? 'prod',
+      awsEndpoint: process.env.AWS_ENDPOINT ?? 'http://localhost:4566'
     })
   ]
 })
